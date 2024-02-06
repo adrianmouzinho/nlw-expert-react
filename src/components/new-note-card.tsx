2e3b5e9aec988b1798d0440d79f9790c9563d7d6
Willame Mouzinho <willame.dev@gmail.com>
2024-02-06 10:16:07 -0300

    feat: add home page

---
+++ src/components/new-note-card.tsx
@@ -0,0 +1,10 @@
+export function NewNoteCard() {
+  return (
+    <div className="bg-slate-700 rounded-md p-5 space-y-3">
+      <span className="text-sm font-medium">Adicionar nota</span>
+      <p className="text-sm leading-6 text-slate-400">
+        Grave uma nota em áudio que será convertida para texto automaticamente.
+      </p>
+    </div>
+  )
+}
